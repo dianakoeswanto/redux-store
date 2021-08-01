12a7d29bc8370c632fc2879e014b28c51cf51a14
Diana Koeswanto <d.koeswanto@gmail.com>
2021-08-01 13:46:05 +1000

    Added redux

---
+++ client/src/utils/StoreContext.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Provider } from 'react-redux';
+
+// Import Redux store from the file we created in /utils
+import store from './store';
+
+const StoreProvider = (props) => {
+  return <Provider store={store} {...props} />;
+};
+
+export default StoreProvider;
